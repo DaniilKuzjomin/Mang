bb79f9a24748588d7211e3354ca3d4e866ae3ac9
opilane <opilane@HP-CZC6297B8F>
2022-09-13 11:45:15 +0300

    Add project files.

---
+++ Mang/Ese.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mang
+{
+    class Ese : IUksus // класс Ese реализует интерфейс IUksus
+    {
+
+        private int punktideArv; // частное свойство punktideArv
+         private string nimi; // частное свойство nimi
+
+        public Ese(int punktideArv, string nimetus) // Конструктор с двумя параметрами по которым можно определить Имя и кол-во Точек
+        {
+            this.punktideArv = punktideArv;
+            this.nimi = nimetus;
+        }
+
+
+        public string info() { return nimi; } // метод info возвращает Имя
+        public int PunktideArv() { return punktideArv; } // метод PunktideArv возвращает Кол-во строчек
+    }
+}
---
+++ Mang/Tegelane.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mang
+{
+    class Tegelane : IUksus // Класс Tegelane реализует интерфейс IUksus
+    {
+        public string nimi; // частное свойство nimi
+        List<Ese> item; // список элементов List<Ese>
+
+        public Tegelane(string nimi) // конструктор с одним параметром который используется для установки имени
+        {
+            this.nimi = nimi;
+        }
+
+
+
+        public string info()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PunktideArv()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void lisaEse() // метод lisaEse который запоминает элемент переданный в качестве элемента
+        {
+            Tegelane tegelane = new Tegelane(nimi);
+        }
+
+        public void punktideArv()
+        {
+
+
+        }
+
+    }
+}
