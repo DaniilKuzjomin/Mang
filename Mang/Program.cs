a6a816e13ca14d975be4674e39a10d94b074ecd5
Daniil Kuzjomin <93324329+DaniilKuzjomin@users.noreply.github.com>
2022-09-14 08:36:51 +0300

    upd

--- Mang/Program.cs
+++ Mang/Program.cs
@@ -1,1 +1,7 @@
-﻿using Mang;
+using KontrolltooMang;
+using System.Runtime.InteropServices;
+
+Ese ese = new Ese("Aleksei", 20);
+Tegelane tegelane = new Tegelane("Men");
+ese.info();
+tegelane.info();
